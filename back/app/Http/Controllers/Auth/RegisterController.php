3dd47c7bb256209ed9c20f223301b335b5f6fbdd
Kival <fa.vakurov@gmail.com>
2024-09-11 07:42:32 +0500

    feat(RegisterController): добавлена валидация
    добавлена валидация принимаемых данных для регистрации

--- back/app/Http/Controllers/Auth/RegisterController.php
+++ back/app/Http/Controllers/Auth/RegisterController.php
@@ -12,6 +12,12 @@ class RegisterController extends Controller
 {
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => ['required', 'string'],
+            'email' => ['required', 'sstring', 'email', 'unique:users'],
+            'password' => ['reqired', 'confirmed', 'min:3']
+        ]);
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
