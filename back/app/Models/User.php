396c0fb07c6cea55f8ec50cd52f54fbbea2b39ca
Kival <fa.vakurov@gmail.com>
2024-09-30 13:44:08 +0500

    feat: создал сортировку для постов пользователя

--- back/app/Models/User.php
+++ back/app/Models/User.php
@@ -50,6 +50,7 @@ class User extends Authenticatable
     public function posts()
     {
         return $this->hasMany(Post::class)
-            ->with('category');
+            ->with('category')
+            ->orderBy('created_at', 'desc');
     }
 }
